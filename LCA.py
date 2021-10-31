237f1e3e07f920c4151d6ff2c042c97b3eab61d0
Xiaoleiii <78795713+Xiaoleiii@users.noreply.github.com>
2021-10-31 21:22:51 +0800

    change to work for DAG

--- LCA.py
+++ LCA.py
@@ -1,45 +1,59 @@
-class LCA(object):
-    class Node(object):
-        def __init__(self, val):
-            self.val = val
-            self.left = None
-            self.right = None
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.children = []
+        self.parents = []
 
-    class BST:
-        def __init__(self):
-            self.root = None
+def addChild(node,child):
+    node.children.append(child)
 
-        def get_node(self, key):
-            return self.get_node1(self.root, key)
+def addParent(node,parent):
+    node.parents.append(parent)
 
-       
-        def get_node1(self, node, key):
-           
-            if node is None or node.key == key:
-                return node
+def findLCA(root,node1,node2):
+    if root is None:
+        return False
+    path1 = []
+    path2 = []
+    findPath(root,node1,path1)
+    findPath(root,node2,path2)
+    lca =0
 
-            
-            if key > node.key:
-                return self.get_node1(node.right, key)
+    for i in path1:
+        for j in path2:
+            if i.val==j.val:
+                lca = i.val
+    return lca
 
-            return self.get_node1(node.left, key)
+def findPath(root,a,path):
+    path.append(root)
+    if a.val==root.val:
+        return True
 
-        def LCA(self, p, q):
-            return self.LCA1(self.root, p, q)
+    for i in root.children:
+        if findPath(i,a,path):
+            return True
 
-        def LCA1(self, root, p, q):
+    path.pop()
+    return False
 
-            if root in (None, p, q):
-                return root
+def findAnc(root,node,anc):
+    anc.append(node.val)
+    if(node.val==root.val):
+        return
 
-            left = self.LCA(root.left, p, q)
-            right = self.LCA(root.right, p, q)
+    for i in node.parents:
+        findAnc(root,i,anc)
 
-            if not left:
-                return right
+def DAG(root,node1,node2):
+    if root is None:
+        return False
 
-            if not right:
-                return left
-
-            if left and right:
-                return root
+    anc = []
+    ancestors2 = []
+    findAnc(root,node1,anc)
+    findAnc(root,node2,ancestors2)
+    for i in anc:
+        for j in ancestors2:
+            if i== j:
+                return i
